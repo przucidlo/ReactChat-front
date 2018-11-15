0d39a1b2ea878c919ef2683ad7d67c6073be7531
abbl <patrykrzucidlo1@gmail.com>
2018-11-15 11:30:56 +0100

    Removing old code

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './misc/index.css';
-import registerServiceWorker from './registerServiceWorker';
-import Chat from './App'
-
-ReactDOM.render(<Chat/>,document.getElementById('root'));
-registerServiceWorker();

d7efdfc75d5bc1cb41a0dcead8060abddb02c1c6
abbl <patrykrzucidlo1@gmail.com>
2018-11-29 12:14:54 +0100

    Fixed a bug with ChatRoom not receiving packet with new ChatMessage.

--- src/app/chatroom/ChatRoomAPI.js
+++ src/app/chatroom/ChatRoomAPI.js
@@ -1,16 +1,25 @@
 
 export const subscribeChatRoom = (socketSubscribe, addMessage, chatRoomId) => {
-    socketSubscribe('/user/topic/chatroom/' + chatRoomId, {id: 'chatroom-' + chatRoomId}, (payload) => {
-        let messageJson = JSON.parse(payload.body);
-    
-        if(!(messageJson instanceof Array)){
-            messageJson = [messageJson];
-        }
-        
-        messageJson.forEach(message => {
-            addMessage(chatRoomId, message.id, message.author, message.content, message.time);            
-        });
+    socketSubscribe('/user/topic/chatroom/' + chatRoomId, {id: 'user-chatroom-' + chatRoomId}, (payload) => {
+        handleMessageResponse(addMessage, payload, chatRoomId)
     })
+
+    socketSubscribe('/topic/chatroom/' + chatRoomId, {id: 'chatroom-' + chatRoomId}, (payload) => {
+        handleMessageResponse(addMessage, payload, chatRoomId);
+    })
+}
+
+const handleMessageResponse = (addMessage, payload, chatRoomId) => {
+    let messageJson = JSON.parse(payload.body);
+    
+    if(!(messageJson instanceof Array)){
+        messageJson = [messageJson];
+    }
+    messageJson.reverse();
+
+    messageJson.forEach(message => {
+        addMessage(chatRoomId, message.id, message.author, message.content, message.time);            
+    });
 }
 
 export const fetchLastMessages = (socketSend, chatRoomId) => {
